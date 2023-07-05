31545940d6131836d852325bf1ed8152a31884e1
Olive14 <Olive14@localhost.local.domain>
2023-07-05 21:43:39 +0900

    Implemented add calculation app.
    ・ボタン押下時に足し算を行う実装をコンポーネントに実装しました。
    ・ボタンのclickイベント発生時に、コンポーネントで定義したメソッドをコールできるようにしました。
    ・テキストフォームに入力されたテキストを受け取れるようにapp.module.tsにFormsModuleの記載を追加しました。

--- angular-app/src/app/simple-form/simple-form.component.ts
+++ angular-app/src/app/simple-form/simple-form.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./simple-form.component.css']
 })
 export class SimpleFormComponent {
+  result:string = "足し算をします";
+  text1!:string;
+  text2!:string;
 
+  //CALCボタンをclickしたときにコールされる
+  addShow():void {
+    let calcRecult:string = "正しい値を入力してください"
+
+    //テキストボックスに入力された文字列を数字に変換する
+    let num1:number = Number(this.text1);
+    let num2:number = Number(this.text2);
+    
+    //各オペランドが数字であれば（NaN：Not a Number  でなければ）計算する
+    if(!Number.isNaN(num1) && !Number.isNaN(num2)) {
+      calcRecult = `${num1} + ${num2} = ${num1 + num2}`;
+    }
+    
+    this.result = calcRecult;
+  }
 }
