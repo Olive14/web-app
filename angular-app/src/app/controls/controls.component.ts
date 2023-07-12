ba2a4c4ad8852563886659258718935548236d42
Olive14 <Olive14@localhost.local.domain>
2023-07-12 23:39:19 +0900

    Created input form by using FormBuilder.
    controls: FormBuilderを使ったアプリ

---
+++ angular-app/src/app/controls/controls.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-controls',
+  templateUrl: './controls.component.html',
+  styleUrls: ['./controls.component.css']
+})
+export class ControlsComponent {
+  //テンプレートからcoffeeFormというプロパティで参照できるようにする
+  coffeeForm!:FormGroup;
+
+  //DI。コンストラクタインジェクション。
+  //どこでもこのfbという変数をプロパティとして用いることができる。、
+  constructor(private fb: FormBuilder) {
+    this.coffeeForm = this.fb.group(
+      {
+        name:"ブレンド",                   //テンプレートにある品名：nameと対応
+        taste:"バランスの良い口当たり"      //テンプレートにあるテイスト：tasteと対応
+      }
+    )
+  }
+}
