6bff2d48154c2db333d51e0c287ae62c62ec782b
Olive14 <Olive14@localhost.local.domain>
2023-07-15 23:52:07 +0900

    Modified radio button part description with *ngFor.
    *ngForディレクティブを使ってラジオボタンの要素を表示するように改良しました。
    これにより、ラジオボタンの要素数が増えたとしても、テンプレートのhtmlに修正を加える必要が無くなります。

--- angular-app/src/app/controls/controls.component.ts
+++ angular-app/src/app/controls/controls.component.ts
@@ -10,6 +10,9 @@ export class ControlsComponent {
   //テンプレートからcoffeeFormというプロパティで参照できるようにする
   coffeeForm!:FormGroup;
 
+  //Hot・Coldの選択肢のラジオボタン要素を配列に格納する
+  hotcoldsel = ["Hot", "Cold"];
+
   //DI。コンストラクタインジェクション。
   //どこでもこのfbという変数をプロパティとして用いることができる。、
   constructor(private fb: FormBuilder) {
@@ -17,7 +20,7 @@ export class ControlsComponent {
       {
         name:"ブレンド",                   //テンプレートにある品名：nameと対応
         taste:"バランスの良い口当たり",     //テンプレートにあるテイスト：tasteと対応
-        hotcold:"Hot"                     //Hot・Coldのラジオボタン
+        hotcold:this.hotcoldsel[0]        //Hot・Coldのラジオボタン。0番目のインデクスを使って初期化。
       }
     )
   }
